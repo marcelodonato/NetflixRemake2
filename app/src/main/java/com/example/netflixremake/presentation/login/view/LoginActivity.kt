56cb50c8caedb5c71d7efae7701775db200f2aba
Marcelo Donato <marcelo_donatoo@hotmail.com>
2023-05-16 16:05:55 -0300

    adjusts code

--- app/src/main/java/com/example/netflixremake/presentation/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/netflixremake/presentation/login/view/LoginActivity.kt
@@ -1,10 +1,19 @@
 package com.example.netflixremake.presentation.login.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.netflixremake.R
 import com.example.netflixremake.base.BaseActivity
 import com.example.netflixremake.base.BaseViewModel
 import com.example.netflixremake.databinding.ActivityLoginBinding
+import com.example.netflixremake.extension.confirmIfEmailIsValid
+import com.example.netflixremake.extension.getEditText
+import com.example.netflixremake.extension.validate
 import com.example.netflixremake.extension.viewBinding
+import com.example.netflixremake.presentation.home.view.HomeActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : BaseActivity<BaseViewModel>() {
 
@@ -15,8 +24,52 @@ class LoginActivity : BaseActivity<BaseViewModel>() {
         setupView()
     }
 
-    private fun setupView(){
+    private fun setupView() {
+        with(binding) {
+            binding.btnLogin.setOnClickListener {
+                validateFields()
+            }
+        }
+    }
 
+    private fun validateFields() {
+        val email = binding.etLogin.validate()
+        val confirmEmail = confirmIfEmailIsValid(binding.etLogin.getEditText())
+        val password = binding.etPassword.validate()
+
+        when {
+            email -> binding.etLogin.error =
+                getString(R.string.generic_error_edit_text, getString(R.string.email))
+            confirmEmail -> binding.etLogin.error = getString(R.string.email_error)
+            password -> binding.etPassword.error = getString(R.string.password_error)
+            else -> {
+//                binding.incLoader.frameLoadingBackground.visibility = View.VISIBLE
+                loginFirebase(
+                    binding.etLogin.getEditText(),
+                    binding.etPassword.getEditText()
+                )
+            }
+        }
+    }
+
+    private fun loginFirebase(email: String, password: String) {
+        val firebase = FirebaseAuth.getInstance()
+        firebase.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    startHomeActivity()
+                } else {
+                    Toast.makeText(this, "Account not found", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, it.message.toString(), Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun startHomeActivity() {
+        val homeIntent = Intent(this, HomeActivity::class.java)
+        startActivity(homeIntent)
+        finish()
     }
 
 }
